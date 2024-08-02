75fde18a7ef4e2d425474d22786db0c9260eebb5
Alireza <alirezasaadat056@gmail.com>
2024-08-02 12:33:17 -0700

    snake movement

--- index.js
+++ index.js
@@ -3,69 +3,83 @@ import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
-class Sphere extends THREE.Mesh{
-    constructor({x , y , z} , radius , color = "#00ff00"){
-        super(
-            new THREE.SphereGeometry(radius , 32 , 32),
-            new THREE.MeshStandardMaterial({color})
-        );
-        this.radius = radius;
-        
-        this.x = x;
-        this.y = y;
-        this.z = z;
+class Sphere extends THREE.Mesh {
+  constructor({ x, y, z }, radius, color = "#00ff00") {
+    super(
+      new THREE.SphereGeometry(radius, 32, 32),
+      new THREE.MeshStandardMaterial({ color })
+    );
+    this.radius = radius;
 
-        this.position.set(x , y , z);
-        this.pos = this.position;
+    this.x = x;
+    this.y = y;
+    this.z = z;
 
-        this.top = this.position.y + this.radius;
-        this.bottom = this.position.y - this.radius;
-        this.right = this.position.x + this.radius;
-        this.left = this.position.x - this.radius;
-        this.front = this.position.z + this.radius;
-        this.back = this.position.z - this.radius;
-    }
+    this.position.set(x, y, z);
+    this.pos = this.position;
+
+    this.top = this.position.y + this.radius;
+    this.bottom = this.position.y - this.radius;
+    this.right = this.position.x + this.radius;
+    this.left = this.position.x - this.radius;
+    this.front = this.position.z + this.radius;
+    this.back = this.position.z - this.radius;
+  }
 }
 
-class Snake{
-    constructor(){
-        this.parts = [
-            new Sphere({x : 0 , y : 0 , z : 0} , 1 , 0x00ff00) ,
-            new Sphere({x : 2 , y : 0 , z : 0} , 1 , 0x00ff00) ,
-            new Sphere({x : 4 , y : 0 , z : 0} , 1 , 0x00ff00)
-        ];
-        this.up = true;
-        this.down = false;
-        this.right = false;
-        this.left = false;
+class Snake {
+  constructor() {
+    this.parts = [
+      new Sphere({ x: 0, y: 0, z: 0 }, 1, 0x00ff00),
+      new Sphere({ x: 2, y: 0, z: 0 }, 1, 0x00ff00),
+      new Sphere({ x: 4, y: 0, z: 0 }, 1, 0x00ff00),
+    ];
+    this.up = true;
+    this.down = false;
+    this.right = false;
+    this.left = false;
 
-        this.unit = 2;
-    }
-    move(){
-        scene.remove(this.parts[0])
-        this.parts.shift();
+    this.unit = 2;
+  }
 
-        this.head = this.parts[this.parts.length - 1];
-        if(this.up){
-            var sphere = new Sphere({x:this.head.x , y:this.head.y , z:this.head.z + this.unit} , 1)
-        }else{
-            var sphere = new Sphere({x:this.head.x , y:this.head.y , z:this.head.z - this.unit} , 1)
-        }
-        if (this.right){
-            var sphere = new Sphere({x:this.head.x + this.unit , y:this.head.y , z:this.head.z} , 1)
-        }else{
-            var sphere = new Sphere({x:this.head.x + this.unit , y:this.head.y , z:this.head.z} , 1)
-        }
-        this.parts.push(sphere)
-        sphere.castShadow = true;
-        scene.add(sphere);
+  move() {
+    scene.remove(this.parts[0]);
+    this.parts.shift();
+
+    this.head = this.parts[this.parts.length - 1];
+    if (this.up) {
+      var sphere = new Sphere(
+        { x: this.head.x, y: this.head.y, z: this.head.z - this.unit },
+        1
+      );
     }
+    if (this.down) {
+      var sphere = new Sphere(
+        { x: this.head.x, y: this.head.y, z: this.head.z + this.unit },
+        1
+      );
+    }
+    if (this.right) {
+      var sphere = new Sphere(
+        { x: this.head.x + this.unit, y: this.head.y, z: this.head.z },
+        1
+      );
+    }
+    if (this.left) {
+      var sphere = new Sphere(
+        { x: this.head.x - this.unit, y: this.head.y, z: this.head.z },
+        1
+      );
+    }
+    this.parts.push(sphere);
+    // sphere.castShadow = true;
+    scene.add(sphere);
+  }
 }
-
 
 // Renerer
-const renderer = new THREE.WebGLRenderer({ antialias : true});
-renderer.setSize(width , height);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
@@ -73,45 +87,67 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75 , width/height , 0.01 , 1000);
-camera.position.set(6 , 20 , 22);
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.01, 1000);
+camera.position.set(6, 30, 20);
 
-const controls = new OrbitControls(camera , renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
 const snake = new Snake();
 
 snake.parts.forEach((part) => {
-    part.castShadow = true;
-    scene.add(part);
+  part.castShadow = true;
+  scene.add(part);
 });
 
 const groundMesh = new THREE.Mesh(
-    new THREE.BoxGeometry(30 , 1 , 30),
-    new THREE.MeshStandardMaterial({color: 0xffffff})
+  new THREE.BoxGeometry(42, 1, 42),
+  new THREE.MeshStandardMaterial({ color: 0xffffff })
 );
 groundMesh.receiveShadow = true;
 groundMesh.position.y = -1.5;
 scene.add(groundMesh);
 
 // Light
-const light = new THREE.DirectionalLight(0xffffff , 1);
-light.position.set(5 , 5 , 5);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 5, 0);
 light.castShadow = true;
 scene.add(light);
 
-const light2 = new THREE.HemisphereLight( 0xffffff , 0x000000 , 1);
+const light2 = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
 scene.add(light2);
- 
-let counter = 0
 
-function animate(){
-    requestAnimationFrame(animate);
-    renderer.render(scene , camera);
-    if (counter % 50 == 0){
-        snake.move()
-    }
-    counter++;
+let counter = 0;
+
+addEventListener("keydown", (event) => {
+  if (event.key == "w" && !snake.down) {
+    snake.right = false;
+    snake.left = false;
+    snake.up = true;
+  } else if (event.key == "s" && !snake.up) {
+    snake.right = false;
+    snake.left = false;
+    snake.down = true;
+  }
+  if (event.key == "a" && !snake.right) {
+    snake.up = false;
+    snake.down = false;
+    snake.left = true;
+  }
+  if (event.key == "d" && !snake.left) {
+    snake.up = false;
+    snake.down = false;
+    snake.right = true;
+  }
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  if (counter % 10 == 0) {
+    snake.move();
+  }
+  counter++;
 }
 animate();

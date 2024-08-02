f2b71dfaff7fc87a94265d811fd71e7871322644
Alireza <alirezasaadat056@gmail.com>
2024-08-02 11:49:55 -0700

    snake movement

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
-
 class Sphere extends THREE.Mesh{
     constructor({x , y , z} , radius , color = "#00ff00"){
         super(
@@ -16,7 +15,8 @@ class Sphere extends THREE.Mesh{
         this.y = y;
         this.z = z;
 
-        this.position.set(x , y , z)
+        this.position.set(x , y , z);
+        this.pos = this.position;
 
         this.top = this.position.y + this.radius;
         this.bottom = this.position.y - this.radius;
@@ -26,6 +26,42 @@ class Sphere extends THREE.Mesh{
         this.back = this.position.z - this.radius;
     }
 }
+
+class Snake{
+    constructor(){
+        this.parts = [
+            new Sphere({x : 0 , y : 0 , z : 0} , 1 , 0x00ff00) ,
+            new Sphere({x : 2 , y : 0 , z : 0} , 1 , 0x00ff00) ,
+            new Sphere({x : 4 , y : 0 , z : 0} , 1 , 0x00ff00)
+        ];
+        this.up = true;
+        this.down = false;
+        this.right = false;
+        this.left = false;
+
+        this.unit = 2;
+    }
+    move(){
+        scene.remove(this.parts[0])
+        this.parts.shift();
+
+        this.head = this.parts[this.parts.length - 1];
+        if(this.up){
+            var sphere = new Sphere({x:this.head.x , y:this.head.y , z:this.head.z + this.unit} , 1)
+        }else{
+            var sphere = new Sphere({x:this.head.x , y:this.head.y , z:this.head.z - this.unit} , 1)
+        }
+        if (this.right){
+            var sphere = new Sphere({x:this.head.x + this.unit , y:this.head.y , z:this.head.z} , 1)
+        }else{
+            var sphere = new Sphere({x:this.head.x + this.unit , y:this.head.y , z:this.head.z} , 1)
+        }
+        this.parts.push(sphere)
+        sphere.castShadow = true;
+        scene.add(sphere);
+    }
+}
+
 
 // Renerer
 const renderer = new THREE.WebGLRenderer({ antialias : true});
@@ -44,13 +80,9 @@ const controls = new OrbitControls(camera , renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-let parts = [
-    new Sphere({x : 0 , y : 0 , z : 0} , 1 , 0x00ff00) ,
-    new Sphere({x : 2 , y : 0 , z : 0} , 1 , 0x00ff00) ,
-    new Sphere({x : 4 , y : 0 , z : 0} , 1 , 0x00ff00)
-];
+const snake = new Snake();
 
-parts.forEach((part) => {
+snake.parts.forEach((part) => {
     part.castShadow = true;
     scene.add(part);
 });
@@ -60,7 +92,7 @@ const groundMesh = new THREE.Mesh(
     new THREE.MeshStandardMaterial({color: 0xffffff})
 );
 groundMesh.receiveShadow = true;
-groundMesh.position.y = -1;
+groundMesh.position.y = -1.5;
 scene.add(groundMesh);
 
 // Light
@@ -71,9 +103,15 @@ scene.add(light);
 
 const light2 = new THREE.HemisphereLight( 0xffffff , 0x000000 , 1);
 scene.add(light2);
+ 
+let counter = 0
 
 function animate(){
     requestAnimationFrame(animate);
     renderer.render(scene , camera);
+    if (counter % 50 == 0){
+        snake.move()
+    }
+    counter++;
 }
 animate();
